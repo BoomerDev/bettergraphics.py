9a8a2d2020df9d07ad1e4df8f2319cfde7e05150
BoomerDev (school) <curiousalt97@gmail.com>
2022-05-17 09:27:37 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='bettergraphics',
+    version='1.0',
+    author='BoomerDev',
+    description='A new and improved version of the old graphics.py module',
+    url='https://github.com/BoomerDev/bettergraphics.py',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License'
+    ],
+    package_dir={'': '.'},
+    packages=['bettergraphics'],
+    python_requires='>=3.6'
+)
